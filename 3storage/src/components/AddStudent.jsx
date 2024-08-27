204eded457e1c478a9e632a2d60e7d80043ab4f1
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-08-27 17:20:27 +0500

    Storage tutorial added

---
+++ 3storage/src/components/AddStudent.jsx
@@ -0,0 +1,87 @@
+// This component is ued To add data to firebase storage.
+
+import { getDatabase, set, ref } from "firebase/database";
+import { useState } from "react";
+import {app} from "../Firebase"
+import { useNavigate } from "react-router-dom";
+
+import {getStorage, ref as employeeRef, uploadBytes, getDownloadURL} from "firebase/storage"
+
+function AddStudent() {
+    const [name, setName] = useState("")
+    const [phone, setPhone] = useState(null)
+    const [admNo, setAdmNo] = useState(null)
+    const navigate = useNavigate()
+
+    // 1
+    const[selectFile, setSelectFile] = useState(null)
+
+
+    // 1
+    const handleFileChange = (e) =>{
+      // console.log(e.target);
+      const file = e.target.files[0]
+      setSelectFile(file)
+    }
+
+    // we will make this function await, because we are sending data to backend.
+    const submitHandler = async(e) =>{
+      e.preventDefault()
+
+      // if file is selected in input field mean true, run this code, and update both the content and image. 
+      // if the file is not selected in input field mean false, run code inside else statement, and update only content.
+      if (selectFile) {
+         // 1
+        // To upload the file to storage.
+        const myStorage = getStorage(app)
+        const myRef = employeeRef(myStorage, 'images/' +admNo)
+
+        // UploadBytes method take two arguments, reference from ref() method and and the file we have stored in the state.
+        await uploadBytes(myRef, selectFile)
+
+        // To send the image url to realtime database as well. and the getDownloadURL() method take one argument, the reference.
+        const imageUrl = await getDownloadURL(myRef)
+
+        const db = getDatabase(app)
+        set(ref(db, 'Students/' +admNo), {
+            studentName: name,
+            studentPhone: phone,
+            // to send the image url to real time database
+            studentImage: imageUrl
+        })
+        .then(res=>{            
+            navigate('/student-list')
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+      }else{
+        const db = getDatabase(app)
+        set(ref(db, 'Students/' +admNo), {
+            studentName: name,
+            studentPhone: phone,
+        })
+        .then(res=>{            
+            navigate('/student-list')
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+      }
+    }
+
+  return (
+    <>
+      <form onSubmit={submitHandler}>
+        <input type="text" placeholder="Student Name" onChange={(e) =>setName(e.target.value)}/>
+        <input type="number" placeholder="Phone No" onChange={(e) =>setPhone(e.target.value)}/>
+        <input type="number" placeholder="Admission No" onChange={(e) =>setAdmNo(e.target.value)}/>
+        {/* 1 */}
+        <input type="file" onChange={handleFileChange} />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+}
+
+export default AddStudent;
---
+++ 3storage/src/components/StudentList.jsx
@@ -0,0 +1,71 @@
+
+import{ getDatabase, ref, onValue, remove} from "firebase/database"
+import {app} from "../Firebase"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+// 1
+import {getStorage, ref as storageRef, deleteObject} from "firebase/storage"
+
+function StudentList(){
+    const [studentData, setStudentData] = useState({})
+    const navigate = useNavigate()
+
+    useEffect( () =>{
+        const db = getDatabase(app)
+        const studenRef = ref(db, 'Students')
+
+        onValue(studenRef, (studentInfo) =>{
+            const data = studentInfo.val()
+            // console.log(data);
+
+            setStudentData(data)
+        })
+    },[])
+
+    const deleteData = (stuId) =>{
+        const db = getDatabase(app)
+        const studenRef = ref(db, 'Students/'+stuId)
+
+        // 1
+        // to delete image from storage.
+        const myStorage = getStorage(app)
+        const myRef = storageRef(myStorage, 'images/'+stuId)
+        // deleteObject() method take on argument, the reference.
+        deleteObject(myRef)
+        // when the image get remove succefully, then remove the other data as well.
+        .then( res=>{
+            remove(studenRef)
+        })
+        .catch(err =>{
+            console.log(err);
+            
+        })
+    }
+
+    return(
+        <div>
+            <h1>Student List</h1>
+            {studentData && (
+                <div className="student-main">
+                    {Object.entries(studentData).map( ([id, obj]) =>{
+                        return(
+                            <div key={id} className="student-data">
+                                <h1>{obj.studentName}</h1>
+                                <p>{obj.studentPhone}</p>
+                                <img src={obj.studentImage} style={{width: "20%", borderRadius: "20px"}}/>
+
+                                <button onClick={() =>{deleteData(id)}}>Delete</button>
+
+                                <button onClick={() =>{navigate("/update-student", {state: [id, obj]})}}>Update</button>
+                            </div>
+                        )
+                    })}
+                </div>
+            )}
+        </div>
+
+    )
+}
+
+export default StudentList;
