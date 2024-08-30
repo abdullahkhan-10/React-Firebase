e7bee456da0c6fbb63888d92c23ed7c5f61dac43
abdullahkhan-10 <abdullahkhanafridi068@gmail.com>
2024-08-30 22:15:07 +0500

    Logout feature added

--- 5Authentication/src/components/Dashboard.jsx
+++ 5Authentication/src/components/Dashboard.jsx
@@ -1,12 +1,28 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { getAuth, signOut} from "firebase/auth"
+import {app} from "../Firebase"
 
 function Dashboard(){
+    const navigate = useNavigate()
+
+    const logOuT = ()=>{
+        const outAuth = getAuth(app)
+        signOut(outAuth)
+        .then( (res) =>{
+            console.log("user get log out");
+            
+            navigate('/login')
+        })
+    }
+    
     return(
         <div className="main">
             <div className="left">
                 <Link to="/">Back To Home</Link>
                 <Link to="/dashboard/add-faculty">Add Faculty</Link>
                 <Link to="/dashboard/faculty-list">Faculty List</Link>
+                
+                <button type="button" onClick={logOuT}>Logout</button>
             </div>
             <div className="right">
                 <Outlet/>
